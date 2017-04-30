77a8a0512190dd98103bc1272793aeb0c7324559
Pikapal101 <annie.pikapal@gmail.com>
2017-04-30 14:28:15 -0400

    Added two <empty> routes to add and remove a course from DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+	root 'plans#index'
+	match '/plans/:plan_id/addcourse/:id', to: 'term#addcourse', via: [:post]
+	match '/plans/:plan_id/removecourse/:id', to: 'term#removecourse', via: [:post]
   resources :courses
   resources :plans
   devise_for :users, :controllers => {:registrations => 'registrations'}
-  root 'plans#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
